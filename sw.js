040e175652c51c04ee8b0ee29ecbf05210f77174
Lisboa <360stv@gmail.com>
2026-06-24 17:01:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const CACHE = 'benjaminlisboa-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.png'];
+const CACHE = 'benjax-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match('./index.html')))
   );
 });
